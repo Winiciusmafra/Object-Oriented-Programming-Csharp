64866be1ec80420153ec9bde7bf76a0557112fc0
winiciusmafra <winicius.mafra@gmail.com>
2021-11-20 23:43:46 -0300

    first commit

---
+++ Object-Oriented-Programming-CSharp/Object-Oriented-Programming-CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Object_Oriented_Programming_CSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
