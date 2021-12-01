0da56ad94c86c279808b86867de594d289440e41
winiciusmafra <winicius.mafra@gmail.com>
2021-12-01 05:47:55 -0300

    update 0.5 - Conditional: if

---
+++ Hello World/Hello World/Conditional/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Learning_csharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("----IF----");
+
+
+            int age = 20;
+            int amountPeople = 3;
+
+            if (age >= 18)
+            {
+                Console.WriteLine("You are older than 18");
+                Console.WriteLine("Wellcome");
+            }
+            else
+            {
+                if (amountPeople >= 2)
+                {
+                    Console.WriteLine("you are not 18 years old, but you can enter, you are accompanied");
+
+                }
+                else
+                {
+                    Console.WriteLine("sorry, but you can't join");
+                }
+            }
+
+            
+        }
+    }
+}
