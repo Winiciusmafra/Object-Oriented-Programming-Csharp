1c3da713664f33d548ffcc940ce74ccce500d0de
winiciusmafra <winicius.mafra@gmail.com>
2021-12-06 06:06:57 -0300

    update 1.0 - Class: First

---
+++ Hello World/Hello World/ByteBank/CurrentAccount.cs
@@ -0,0 +1,8 @@
+﻿public class CurrentAcconut
+{
+    public string holder;
+    public int agency;
+    public int currentAccount;
+    public double balance;
+
+}
---
+++ Hello World/Hello World/ByteBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Learning_csharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new CurrentAcconut();
+
+            Console.ReadLine();
+
+        }
+    }
+}
