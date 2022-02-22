db547b9627596fdd943c3262005042e5b9305974
winiciusmafra <winicius.mafra@gmail.com>
2022-02-22 20:32:00 -0300

    update 2.0 - Inheritance and Interface: employee class

---
+++ Hello World/Hello World/Relations/Employees/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relations.Employees
+{
+    public class Employee
+    {
+        // 1 - Director
+        // 2 - Designer
+        // 3 - Employee
+        private int _office;
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int office)
+        {
+            this._office = office;
+        }
+
+        public double GetBonus()
+        {
+            if (_office == 1)
+            {
+                return Salary;
+            }
+            return Salary * 0.10;
+        }
+
+
+
+    }
+}
---
+++ Hello World/Hello World/Relations/Program.cs
@@ -0,0 +1,21 @@
+﻿using Relations.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Learning_csharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee winicius = new Employee(1);
+            winicius.Name = "Winicius";
+            winicius.Cpf = "46743576813";
+            winicius.Salary = 1000;
+            Console.WriteLine(winicius.GetBonus());
+
+            Console.ReadLine();
+        }
+    }
+}
